c06f6105f5c354514ff0b5752cdcc7c2fe5d5688
Karlos Zurutuza <55366904+torfoes@users.noreply.github.com>
2021-09-04 18:25:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,17 +5,18 @@ import string
 import time
 
 from threading import Thread
-
 from instagram_web_api import Client, ClientError, ClientLoginError
 
-
-# raise queryError:
-#     print("Error getting a response froom instagram.")
-
+# how often a query is sent to instagram to see if the post has been archived, or deleted
 QUERY_TIME = 30
 
-class MyClient(Client):
+# number of posts that you would like to get from instagram (from most recent) - cannot be more than fifty
+NUM_QUERY_POSTS = 10
 
+EMAIL_FOR_NOTIFICATION = "karloszuru@gmail.com"
+
+# this class here because I was having issues with the built in client from the insta_api client
+class MyClient(Client):
     @staticmethod
     def _extract_rhx_gis(html):
         options = string.ascii_lowercase + string.digits
@@ -42,34 +43,38 @@ class MyClient(Client):
         return login_res
 
 
-
-
 class userBase:
+    """Main class that holds users and allows you to perform different operations on them"""
     def __init__(self):
         self.user_dict = {}
         self.web_api = MyClient()
 
     def display(self):
+        """Displays all a user's posts that were returned from the original query"""
         for user in self.user_dict:
             print("User: " + user)
             for post in self.user_dict[user]:
                 print(post['node'])
 
     def all_users(self):
+        """Displays all usernames saved in the dictionary"""
         for user in self.user_dict:
             print("User: " + user)
 
-    def add_user(self, user_name):
-        new_user = user(user_name)
+    def add_user(self):
+        """Adds a user class to the dictionary with all of their information.
+        Usually a helper function called by the add_tracked_post."""
+        username = input("Input user to track: ")
+        new_user = user(username)
 
         if new_user is None:
-            print("None returned from query........")
-            exit()
+            print("Nothing returned from query or something else :3")
             return
         else:
-            self.user_dict[user_name] = new_user
+            self.user_dict[username] = new_user
             return new_user
 
+<<<<<<< Updated upstream
     def add_tracked_post(self, user_name):
         try:
             tracked_user = self.add_user(user_name)
@@ -82,9 +87,18 @@ class userBase:
 
         except:
             print("Creation of user class failed.")
+=======
+    def add_tracked_post(self):
+        """creates a new user based input username"""
+        tracked_user = self.add_user()
+
+        print("Which post would you like to track: ")
+        tracked_user.display_posts()
+        post_index = int(input("Enter post index: "))
+>>>>>>> Stashed changes
 
 
-    def foo(self):
+    def periodic_check(self):
         while True:
             for user_key in self.user_dict:
                 current_user = self.user_dict[user_key]
@@ -92,71 +106,88 @@ class userBase:
 
                 tracked_post_id = current_user.posts[tracked_post_number]['node']['id']
 
-                print("Checking user:", current_user.user_name, "post:", tracked_post_id)
+                print("Checking for user:", current_user.username, "post id:", tracked_post_id)
 
                 curr_user_info = current_user.get_posts()
-                # print(curr_user_info)
 
                 post_exists = False
 
                 for post in curr_user_info:
                     current_post_id = post['node']['id']
-                    print("Checking: ", current_post_id)
+
                     if current_post_id == tracked_post_id:
                         post_exists = True
 
                 if not post_exists:
                     print("Post", tracked_post_id, "was deleted.")
 
-                    # user_info = self.user_dict[user_id]
-                    #
-                    # print(user_info)
-                    #
-                    # post_exists = True
-
-                    # for post in user_info['node']:
-                    #     if post['id'] == post_info[1]:
-                    #         print("Post no longer exists.")
-                    #         post_exists = False
-
             time.sleep(QUERY_TIME)
 
 
 class user(userBase):
-    def __init__(self, user_name):
+    def __init__(self, username):
         super().__init__()
-        self.user_name = user_name
+        self.username = username
         self.profile = self.get_profile()
         self.id = self.get_id()
-        self.posts = self.get_posts()
+        self.posts = self.save_posts()
         self.tracked_posts = []
 
     def get_profile(self):
+<<<<<<< Updated upstream
         user_url = 'https://www.instagram.com/%s/?__a=1' % self.user_name
         self.web_api._make_request(user_url)
         return
+=======
+        """Gets user profile from username."""
+        print("Getting profile information.")
+        user_url = 'https://www.instagram.com/%s/?__a=1' % self.username
+        try:
+            return self.web_api._make_request(user_url)
+        except:
+            print("User does not exist.")
+            return
+>>>>>>> Stashed changes
 
     def get_id(self):
+        """Strips user ID from profile information"""
         user_id = self.profile["logging_page_id"]
         user_id = user_id[12:]
         return user_id
 
     def get_posts(self):
+<<<<<<< Updated upstream
         print("Getting %s (%s) posts." % (self.user_name, self.id))
         self.posts = self.web_api.user_feed(self.id)
         # print("Error in querying instagram.")
         return self.posts
+=======
+        """Uses user ID to get all CURRENT posts from a user without updating saved posts."""
+        try:
+            current_posts = self.web_api.user_feed(self.id, count=NUM_QUERY_POSTS)
+            return current_posts
+        except:
+            print("Error in querying instagram.")
+            return
+>>>>>>> Stashed changes
+
+    def save_posts(self):
+        print("Saving posts to current user.")
+        return self.get_posts()
 
     def display_posts(self):
-        print("Displaying %s (%s) posts:" % (self.user_name, self.id))
+        """Displays all of the posts of a user (up to fifty)"""
+        print("Displaying %s (%s) posts:" % (self.username, self.id))
         for index, post in enumerate(self.posts):
             print(index, post)
 
-    def track(self, post_num):
-        self.tracked_posts.append(post_num)
+    def track(self, post_id):
+        """Adds post to tracked posts list"""
+        self.tracked_posts.append(post_id)
 
 
 if __name__ == '__main__':
+<<<<<<< Updated upstream
     # a = MyClient()
     #
     # user_name = input("Enter username fucker: ")
@@ -194,3 +225,11 @@ if __name__ == '__main__':
     # users.add_tracked_post('199414232')
     # users.add_user('10448683263')
     # users.add_tracked_post('10448683263')
+=======
+    users = userBase()
+    users.add_tracked_post()
+    users.add_tracked_post()
+
+    user_add()
+    Thread(target=users.periodic_check).start()
+>>>>>>> Stashed changes
